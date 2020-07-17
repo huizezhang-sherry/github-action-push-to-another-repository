9eabc13e276c61e308bb7db13b67fd69fc0365dc
Emi Tanaka <dr.emi.tanaka@gmail.com>
2020-07-17 21:00:45 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,8 @@ git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
 git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
 
-ls -la "$CLONE_DIR"
-
 # Copy files into the git and deletes all git
-find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
-
-ls -la "$CLONE_DIR"
+find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs sudo rm -rf # delete all files (to handle deletions)
 
 cp -r "$FOLDER"/* "$CLONE_DIR"
 

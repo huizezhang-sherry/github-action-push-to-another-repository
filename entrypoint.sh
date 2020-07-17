51e44b21483b33144afe86bd5f0b3f580fa6646f
Emi Tanaka <dr.emi.tanaka@gmail.com>
2020-07-18 05:55:04 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ CLONE_DIR=$(mktemp -d)
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
-git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
+git clone --single-branch --branch gh-pages "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
 
 # Copy files into the git and deletes all git
 sudo find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
@@ -23,5 +23,5 @@ cd "$CLONE_DIR"
 
 git add .
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA)"
-git branch "$DST_BRANCH"
+# git branch "$DST_BRANCH"
 git push origin "$DST_BRANCH"

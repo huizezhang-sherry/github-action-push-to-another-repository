f07d10efd36e8d75f6956000591aedc2a11ba7ce
Emi Tanaka <dr.emi.tanaka@gmail.com>
2021-07-12 16:51:38 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,21 +7,12 @@ GITHUB_REPO="$3"
 USER_EMAIL="$4"
 DST_BRANCH="$5"
 
-CLONE_DIR=$(mktemp -d)
-
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
-git clone --single-branch --branch "$DST_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
-
-# Copy files into the git and deletes all git
-sudo find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
-
-cp -r "$FOLDER"/* "$CLONE_DIR"
-
-cd "$CLONE_DIR"
-
-git add .
-git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA)"
-# git branch "$DST_BRANCH"
-git push origin "$DST_BRANCH"
+rm -rf .git
+git init
+git add . 
+git commit -m "build website"
+git remote add origin "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git"
+git push -u --force origin "$DST_BRANCH"
